04749a881877afb676f6c73c4d312e1533d25946
lifejava <vorkout2013@gmail.com>
2016-05-14 15:07:16 +0300

    Алгоритм нахождения наибольшего числа

---
+++ src/chapter1/Example1.java
@@ -0,0 +1,21 @@
+package chapter1;
+
+public class Example1 {
+  
+  public int findLargest(int[] arr) {
+    int largest = arr[0];
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] > largest) {
+	largest = arr[i];
+      }
+    }
+    return largest;
+  }
+  
+  
+  public static void main(String[] args) {
+    Example1 example = new Example1();
+    int z = example.findLargest(new int[]{1, 7, 40, 39, 60, 293, 455, 135});
+    System.out.println(z);
+  }
+}
